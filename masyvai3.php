30251234a8a299a8c0e34d41ef9b9ada0643102b
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-16 22:01:29 +0200

    pirmas

---
+++ masyvai3.php
@@ -0,0 +1,10 @@
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 1 ****** </h3>
+
+<?php
+echo "1. Sugeneruokite masyvą iš 10 elementų, <br>
+kurio elementai būtų masyvai iš 5 elementų su reikšmėmis nuo 5 iki 25"; 
+echo '<br>';
+ 
+
+  
+?>
